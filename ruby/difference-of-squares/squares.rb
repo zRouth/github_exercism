9d29a9629f4ecef96fa3a8bf9f1db8add6e4d463
zRouth <zrouthier@gmail.com>
2017-09-04 09:03:39 -0500

    something strange has happened

--- ruby/difference-of-squares/squares.rb
+++ ruby/difference-of-squares/squares.rb
@@ -7,12 +7,7 @@ class Squares
 
   def square_of_sums
     (1.upto(number).reduce(:+))**2
-<<<<<<< HEAD
-  en
-=======
-  end
 
->>>>>>> 1adf96f8dce4a06571fe4988b2e95df552ad8ae6
   def sum_of_squares
     1.upto(number).reduce { |sum, number| sum + number**2 }
   end
--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,7 +1,6 @@
 class Hamming
 
   def self.compute(strand1, strand2)
-<<<<<<< HEAD
     if strand1.length > strand2.length
       strand2.chars.each_with_index.count do |value, index|
         value != strand1[index]
@@ -12,13 +11,4 @@ class Hamming
       end
     end
   end
-=======
-    if strand1.chars == strand2.chars
-      0
-    else
-
-    end
-  end
-
->>>>>>> 1adf96f8dce4a06571fe4988b2e95df552ad8ae6
 end
